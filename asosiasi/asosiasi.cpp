fb12aea8f30712b2ded1f719c28b7eed3c449e56
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-26 23:50:08 +0700

    membuat void pasien

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -33,3 +33,8 @@ public:
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+    pDokter->tambahPasien(this);
+}
