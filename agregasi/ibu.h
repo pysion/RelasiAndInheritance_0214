91e54dd835752a46ba617308ed16b9e19b647b04
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-26 18:38:06 +0700

    membuat liblary ibu

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -1,25 +1,3 @@
 #ifndef IBU_H
 #define IBU_H
 
-class ibu {
-    public:
-        string nama;
-        vector<anak *> daftar_anak;
-
-        ibu(string pNama) : nama (pNama) {
-            cout << "ibu \"" << nama << "\" ada \n";
-        }
-        ~ibu() {
-            cout << "ibu \"" << nama << "\" tidak ada \n";
-        }
-        void tambahkanAnak (anak *);
-        void cetakAnak();
-};
-void ibu::tambahAnak(anak * pAnak) {
-    daftar_anak.push_push (pAnak);
-}
-void ibu::cetakAnak() {
-    cout << "Daftar anak dari ibu \"" << this ->nama << "\":\n";
-}
-
-for (auto &a : daftar_anak)
