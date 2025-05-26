c34305fc04f181f502572e039fe4ff7151d3c868
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-26 18:38:43 +0700

    membuat member ibu

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -2,4 +2,14 @@
 #define IBU_H
 
 class ibu {
-    
+    public:
+        string nama;
+        vector<anak *> daftar_anak;
+
+        ibu(string pNama) : nama (pNama) {
+            cout << "ibu \"" << nama << "\" ada \n";
+        }
+        ~ibu() {
+            cout << "ibu \"" << nama << "\" tidak ada \n";
+        }
+       
