742dc9831e64596ec40502137f260d9c90b3438c
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-26 23:53:46 +0700

    membuat class jantung

--- komposisi/jantung.h
+++ komposisi/jantung.h
@@ -3,4 +3,14 @@
 
 using namespace std;
 
+class jantung {
+public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
 
+#endif
