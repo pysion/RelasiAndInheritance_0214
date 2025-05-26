ee01a511deb6f3a466398033d3785cf97245a055
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-26 23:51:19 +0700

    membuat void dokter cetak pasien

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -50,3 +50,11 @@ void pasien::cetakDokter() {
     }
     cout << endl;
 }
+
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
